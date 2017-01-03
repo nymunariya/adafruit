dbca49d3b8dd77fa4d6a57420f8422ae80f32c3c
nymunariya <nicki.aya@web.de>
2017-01-03 19:55:05 +0100

    Create clockwise_counter.ino

---
+++ circuit_playground/clockwise_counter/clockwise_counter.ino
@@ -0,0 +1,44 @@
+// Clockwise_counter
+// basics of a clock function.
+//
+// Based off mega_demo by Tony DiCola
+//
+// Author: Nymunariya
+// License: GNU GPLv3 (http://www.gnu.org/licenses/gpl-3.0.txt)
+
+#include <Adafruit_CircuitPlayground.h>
+#include <Wire.h>
+
+int colour = 180;
+int counter = 0;
+int ticker = 0;
+bool richtung = true;
+
+static int left_array[5] = {4,3,2,1,0};
+static int right_array[5] = {5,6,7,8,9};
+static int clockwise[10] = {9,8,7,6,5,4,3,2,1,0};
+
+void setup() {
+  Serial.begin(9600);
+  CircuitPlayground.begin();
+  CircuitPlayground.setBrightness(2);
+}
+
+void loop() {
+  Serial.print("Counter: "); Serial.println(counter);
+  Serial.print("Ticker: "); Serial.println(ticker);
+  
+  if( counter > 0 ) CircuitPlayground.setPixelColor(clockwise[counter - 1], 0xFF00FF);
+
+  if( ticker != counter -1 ) CircuitPlayground.setPixelColor(clockwise[ticker], 0x0000FF);
+  else CircuitPlayground.setPixelColor(clockwise[ticker], 0x00FF00);
+
+  delay(1000);
+  CircuitPlayground.clearPixels();
+  
+  if( ticker == 9 ) {
+     ticker = 0;
+     if( counter == 10 ) counter = 0;
+     else counter++;
+  } else ticker++;
+}
